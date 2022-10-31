5321cfe1905cc5a0787ffa80b76c26d6ec07dae5
Shisei Hanai <ruimo.uno@gmail.com>
2022-10-31 15:47:07 +0900

    Sample

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,46 @@
-fn main() {
-    println!("Hello, world!");
+use clap::Parser;
+use lapin::{Result, Connection, ConnectionProperties, options::{QueueDeclareOptions, BasicPublishOptions}, types::FieldTable, BasicProperties};
+use model::User;
+
+#[derive(Parser, Debug)]
+struct Args {
+    #[arg(short, long, default_value="amqp://127.0.0.1:5672/%2f")]
+    ampq_addr: String,
+    #[arg(short, long, default_value="myqueue")]
+    queue_name: String,
+    user_name: String,
+    email: String,
+}
+
+fn main() -> Result<()> {
+    let args = Args::parse();
+    let user = User { user_name: args.user_name.clone(), email: args.email.clone() };
+
+    async_global_executor::block_on(async {
+        let conn = Connection::connect(
+            &args.ampq_addr, ConnectionProperties::default(),
+        ).await?;
+
+        let channel = conn.create_channel().await?;
+
+        let _ = channel
+            .queue_declare(
+                &args.queue_name, QueueDeclareOptions::default(), FieldTable::default(),
+            ).await?;
+
+        let payload = User::serialize(&user).unwrap();
+
+        let _ = channel
+            .basic_publish(
+                "",
+                &args.queue_name,
+                BasicPublishOptions::default(),
+                &payload,
+                BasicProperties::default(),
+            )
+            .await?
+            .await?;
+
+        Ok(())
+    })
 }
