2d4b027f4a65288a6fddd79d6b204e2f09836b51
Shisei Hanai <ruimo.uno@gmail.com>
2022-10-31 16:22:15 +0900

    Refactor

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 use clap::Parser;
-use lapin::{Result, Connection, ConnectionProperties, options::{QueueDeclareOptions, BasicPublishOptions}, types::FieldTable, BasicProperties};
+use lapin::{Result, Connection, ConnectionProperties, options::{BasicPublishOptions}, BasicProperties};
 use model::User;
 
 #[derive(Parser, Debug)]
